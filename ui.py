cbecb2a6f71a67ff6f0dca2612ba6d9cca79f9ca
keaikelly <keaikelly@naver.com>
2023-01-13 22:21:24 +0900

    Modify activateMessage function to add an argument

--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-# ch 5.2.1 ui.py
+# ch 6.3.2 ui.py
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout, QLineEdit, QComboBox) # QLineEdit, QComboBox 추가
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore #모듈추가
@@ -50,8 +50,8 @@ class View(QWidget):
 
         
 
-    def activateMessage(self):
-        self.te1.appendPlainText("Button clicked!")
-
+    def activateMessage(self, text):
+        self.te1.appendPlainText(text)
+        
     def clearMessage(self):
         self.te1.clear()
