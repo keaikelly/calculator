e340a8e4e1084d13ff41ff0baec92de21cab5259
keaikelly <keaikelly@naver.com>
2023-01-17 21:37:19 +0900

    Modify calculate function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -1,4 +1,4 @@
-# ch 7.5.1 ctrl.py
+# ch 7.6.2 ctrl.py
 class Control:
 
     def __init__(self, view):
@@ -6,6 +6,27 @@ class Control:
         self.connectSignals()
 
     def calculate(self): # calculate 메서드 추가. 내용은 추후에 작성
+        try: # 숫자가 아닌 값이 입력되었을 때도 프로그래밍 동작하도록 예외 처리 구문 추가
+            num1 = float(self.view.le1.text())
+            num2 = float(self.view.le2.text())
+            operator = self.view.cb.currentText()
+            # 연산자에 따라 각각 다른 함수를 사용하여 결과를 리턴
+            if operator == '+':
+                return f'{num1} + {num2} = {self.sum(num1, num2)}'
+            elif operator == '-':
+                return f'{num1} - {num2} = {self.sub(num1, num2)}' 
+            elif operator == '*':
+                return f'{num1} * {num2} = {self.mul(num1, num2)}'
+            elif operator == '/':
+                return f'{num1} / {num2} = {self.div(num1, num2)}'
+            elif operator == '^':
+                return f'{num1} ^ {num2} = {self.pow(num1, num2)}'
+            else :
+                return "Calculation Error"
+        except :
+            return "Calculation Error"
+
+
         num1 = float(self.view.le1.text()) # 첫 번째 라인 에디트에 입력된 숫자를 읽어옴
         num2 = float(self.view.le2.text()) # 두 번째 라인 에디트에 입력된 숫자를 읽어옴
         operator = self.view.cb.currentText() # 콤보 박스에 선택된 연산자 확인
